0cd31e866ffc948c6b050b7c66608f9b6348dd6a
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-04-01 22:04:27 +0530

    Create 617A.cpp

---
+++ 617A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int ans = (n + 4) / 5;
+    cout << ans;
+ 
+}
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long int t;
+    // cin >> t;
+    t = 1;
+    while (t--)
+    {
+        solve();
+    }
+}
