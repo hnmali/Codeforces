8cb9eee4de5de37ab001a36efb5a73f710b3bb27
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-04-04 19:18:58 +0530

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,22 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    void solve()
+    {
+        int n, m;
+        cin >> n >> m;
+        cout << (m * n) / 2;
+    }
+    signed main()
+    {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        long long int t;
+        // cin >> t;
+        t = 1;
+        while (t--)
+        {
+            solve();
+        }
+    }
