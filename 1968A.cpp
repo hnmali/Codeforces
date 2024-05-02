a780fab3f217ae508eb99bbc48a4807ed33b5e23
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-05-02 22:00:36 +0530

    Create 1968A.cpp

---
+++ 1968A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n-1 << "\n";
+    return;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long t;
+    cin >> t;
+    while (t--)
+        solve();
+}
