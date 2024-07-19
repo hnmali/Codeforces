8856b068fd781a48b1f63016f8b2421319edd6c1
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-07-19 18:50:38 +0530

    Create 1878A.cpp

---
+++ 1878A.cpp
@@ -0,0 +1,33 @@
+//hnmali
+#include <iostream>
+#include <vector>
+// #include <algorithm>
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == k) {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
