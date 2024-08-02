226e936eaac4f530e6cf51c5837d9beecb185b7e
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-08-02 21:40:04 +0530

    Create 1325A.cpp

---
+++ 1325A.cpp
@@ -0,0 +1,25 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <algorithm>
+using namespace std;
+
+void solve() {
+    int x;
+    cin >> x;
+    cout << "1 " << x-1 << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
