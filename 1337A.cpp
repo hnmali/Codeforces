5b1700a536e725e23f96637ccaf1308e5cc3cb77
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-08-13 23:48:00 +0530

    Create 1337A.cpp

---
+++ 1337A.cpp
@@ -0,0 +1,25 @@
+//hnmali
+#include <iostream>
+#include <vector>
+// #include <algorithm>
+using namespace std;
+
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
