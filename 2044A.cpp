ac5b3de5bf2f535c37a91435fa7fb420ca75ff94
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-12-20 23:26:29 +0530

    Create 2044A.cpp

---
+++ 2044A.cpp
@@ -0,0 +1,28 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <map>
+// #include <unordered_set>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n-1 << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
