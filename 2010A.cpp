9778a8536c1df6a21b8d056b6bcec1bfa5bda8c4
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-01-17 22:15:31 +0530

    Create 2010A.cpp

---
+++ 2010A.cpp
@@ -0,0 +1,38 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <string>
+// #include <map>
+// #include <unordered_set>
+// #include<stack>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    int n, ans = 0;
+    cin >> n;
+    int temp;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        if(i&1)
+            ans -= temp;
+        else
+            ans += temp;
+    }
+    cout << ans << '\n';
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
