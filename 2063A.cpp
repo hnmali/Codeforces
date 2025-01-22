2d79d2c66bbbedc63029376df22ef10c04096cda
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-01-22 23:46:20 +0530

    Create 2063A.cpp

---
+++ 2063A.cpp
@@ -0,0 +1,33 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <string>
+// #include <map>
+// #include <unordered_set>
+// #include<stack>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    int l, r;
+    cin >> l >> r;
+    if( l == r && l == 1) 
+        cout << "1\n";
+    else
+        cout << r - l << '\n';
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
