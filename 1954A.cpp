175c0ed74f6f2574580460c68e5d57d49d18049c
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-01-23 23:41:59 +0530

    Create 1954A.cpp

---
+++ 1954A.cpp
@@ -0,0 +1,34 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <string>
+// #include <map>
+// #include <unordered_set>
+// #include<stack>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    int maxColor = (n+m-1)/m;
+    if(maxColor+k >= n)
+        cout << "NO\n";
+    else
+        cout << "YES\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
