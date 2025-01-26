13d9fef0e4fd791449b6eef9a6d2d33ea428fce2
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-01-26 23:20:10 +0530

    Create 2024A.cpp

---
+++ 2024A.cpp
@@ -0,0 +1,34 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <string>
+// #include <map>
+// #include <unordered_set>
+// #include<stack>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    b = max(0,b-a);
+    if(a-b < 0)
+    	cout << "0\n";
+    else
+    	cout << a-b << '\n';
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
