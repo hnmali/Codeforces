d761ada69affae107aacfd53eba605132e527599
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-01-27 21:44:56 +0530

    Create 2062A.cpp

---
+++ 2062A.cpp
@@ -0,0 +1,34 @@
+//hnmali
+#include <iostream>
+// #include <vector>
+// #include <string>
+// #include <map>
+// #include <unordered_set>
+// #include<stack>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    int ans = 0;
+    for(auto i : s) 
+        if(i == '1')
+            ans++;
+    cout << ans <<'\n';
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
