67064fe90b032a4235d738d595a8482d6a7b48fa
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-02-17 19:03:35 +0530

    Create 1092A.cpp

---
+++ 1092A.cpp
@@ -0,0 +1,36 @@
+//hnmali
+#include <iostream>
+#include <vector>
+// #include <string>
+// #include <map>
+// #include <unordered_set>
+// #include<stack>
+// #include <algorithm>
+// #include <climits>
+using namespace std;
+
+void solve() {
+    int n, k, curr = 0;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        if(curr == k)
+            curr = 0;
+        cout << char(curr+'a');
+        curr++;
+    }
+    cout << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
